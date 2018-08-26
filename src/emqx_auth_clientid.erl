f54dc0a80004f048a732f7873baba21af8759e9e
Feng Lee <feng@emqx.io>
2018-08-26 13:35:06 +0800

    Upgrade the plugin for EMQ X 3.0

--- src/emqx_auth_clientid.erl
+++ src/emqx_auth_clientid.erl
@@ -23,59 +23,62 @@
 %% emqx_auth_mod callbacks
 -export([init/1, check/3, description/0]).
 
--record(mqtt_auth_clientid, {client_id, password}).
+-define(TAB, ?MODULE).
+-record(?TAB, {client_id, password}).
 
-%%--------------------------------------------------------------------
+%%------------------------------------------------------------------------------
 %% API
-%%--------------------------------------------------------------------
+%%------------------------------------------------------------------------------
 
 %% @doc Add clientid with password
 -spec(add_clientid(binary(), binary()) -> {atomic, ok} | {aborted, any()}).
 add_clientid(ClientId, Password) ->
-    R = #mqtt_auth_clientid{client_id = ClientId, password = Password},
+    R = #?TAB{client_id = ClientId, password = Password},
     mnesia:transaction(fun mnesia:write/1, [R]).
 
 %% @doc Lookup clientid
--spec(lookup_clientid(binary()) -> list(#mqtt_auth_clientid{})).
+-spec(lookup_clientid(binary()) -> list(#?TAB{})).
 lookup_clientid(ClientId) ->
-    mnesia:dirty_read(mqtt_auth_clientid, ClientId).
+    mnesia:dirty_read(?TAB, ClientId).
 
 %% @doc Lookup all clientids
 -spec(all_clientids() -> list(binary())).
-all_clientids() -> mnesia:dirty_all_keys(mqtt_auth_clientid).
+all_clientids() ->
+    mnesia:dirty_all_keys(?TAB).
 
 %% @doc Remove clientid
--spec(remove_clientid(binary()) -> {atomic, ok} | {aborted, any()}).
+-spec(remove_clientid(binary()) -> {atomic, ok} | {aborted, term()}).
 remove_clientid(ClientId) ->
-    mnesia:transaction(fun mnesia:delete/1, [{mqtt_auth_clientid, ClientId}]).
+    mnesia:transaction(fun mnesia:delete/1, [{?TAB, ClientId}]).
 
-%%--------------------------------------------------------------------
+%%------------------------------------------------------------------------------
 %% emqx_auth_mod callbacks
-%%--------------------------------------------------------------------
+%%------------------------------------------------------------------------------
 
 init(ClientList) ->
-    ok = ekka_mnesia:create_table(mqtt_auth_clientid, [
+    ok = ekka_mnesia:create_table(?TAB, [
             {disc_copies, [node()]},
-            {attributes, record_info(fields, mqtt_auth_clientid)}]),
-    ok = ekka_mnesia:copy_table(mqtt_auth_clientid, disc_copies),
+            {attributes, record_info(fields, ?TAB)}]),
+    ok = ekka_mnesia:copy_table(?TAB, disc_copies),
     Clients = [r(ClientId, Password) || {ClientId, Password} <- ClientList],
     mnesia:transaction(fun() -> [mnesia:write(C) || C <- Clients] end),
     {ok, []}.
 
 r(ClientId, Password) ->
-    #mqtt_auth_clientid{client_id = iolist_to_binary(ClientId),
-                        password  = iolist_to_binary(Password)}.
+    #?TAB{client_id = iolist_to_binary(ClientId),
+          password  = iolist_to_binary(Password)}.
 
-check(#mqtt_client{client_id = undefined}, _Password, _) ->
+check(#{client_id := undefined}, _Password, _) ->
     {error, clientid_undefined};
-check(_Client, undefined, _) ->
+check(_Credentials, undefined, _) ->
     {error, password_undefined};
-check(#mqtt_client{client_id = ClientId}, Password, _) ->
-    case mnesia:dirty_read(mqtt_auth_clientid, ClientId) of
+check(#{client_id := ClientId}, Password, _) ->
+    case mnesia:dirty_read(?TAB, ClientId) of
         [] -> ignore;
-        [#mqtt_auth_clientid{password = Password}]  -> ok; %% TODO: plaintext??
+        [#?TAB{password = Password}] -> ok; %% TODO: plaintext??
         _ -> {error, password_error}
     end.
 
-description() -> "ClientId authentication module".
+description() ->
+    "ClientId Authentication Module".
 
