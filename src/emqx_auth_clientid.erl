9f96ea10ea38c45903ffe6a230831dd9815c0fa5
Feng Lee <feng@emqx.io>
2019-08-24 16:30:31 +0800

    Update copyright and define 'AUTH_METRICS' macro

--- src/emqx_auth_clientid.erl
+++ src/emqx_auth_clientid.erl
@@ -1,4 +1,5 @@
-%% Copyright (c) 2013-2019 EMQ Technologies Co., Ltd. All Rights Reserved.
+%%--------------------------------------------------------------------
+%% Copyright (c) 2019 EMQ Technologies Co., Ltd. All Rights Reserved.
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -11,6 +12,7 @@
 %% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
+%%--------------------------------------------------------------------
 
 -module(emqx_auth_clientid).
 
@@ -40,11 +42,17 @@
 
 -define(TAB, ?MODULE).
 
+-define(AUTH_METRICS,
+        ['auth.clientid.success',
+         'auth.clientid.failure',
+         'auth.clientid.ignore'
+        ]).
+
 -record(?TAB, {client_id, password}).
 
-%%-----------------------------------------------------------------------------
+%%--------------------------------------------------------------------
 %% CLI
-%%-----------------------------------------------------------------------------
+%%--------------------------------------------------------------------
 
 cli(["list"]) ->
     if_enabled(fun() ->
@@ -81,9 +89,9 @@ if_enabled(Fun) ->
 hint() ->
     emqx_cli:print("Please './bin/emqx_ctl plugins load emqx_auth_clientid' first.~n").
 
-%%------------------------------------------------------------------------------
+%%--------------------------------------------------------------------
 %% API
-%%------------------------------------------------------------------------------
+%%--------------------------------------------------------------------
 
 is_enabled() ->
     lists:member(?TAB, mnesia:system_info(tables)).
@@ -134,9 +142,9 @@ unwrap_salt(<<_Salt:4/binary, HashPasswd/binary>>) ->
 ret({atomic, ok})     -> ok;
 ret({aborted, Error}) -> {error, Error}.
 
-%%------------------------------------------------------------------------------
+%%--------------------------------------------------------------------
 %% Auth callbacks
-%%------------------------------------------------------------------------------
+%%--------------------------------------------------------------------
 
 init() ->
     ok = ekka_mnesia:create_table(?TAB, [
@@ -145,7 +153,7 @@ init() ->
     ok = ekka_mnesia:copy_table(?TAB, disc_copies).
 
 register_metrics() ->
-    [emqx_metrics:new(MetricName) || MetricName <- ['auth.clientid.success', 'auth.clientid.failure', 'auth.clientid.ignore']].
+    [emqx_metrics:new(MetricName) || MetricName <- ?AUTH_METRICS].
 
 check(#{client_id := ClientId, password := Password}, AuthResult, #{hash_type := HashType}) ->
     case mnesia:dirty_read(?TAB, ClientId) of
